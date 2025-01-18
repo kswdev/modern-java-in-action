e03d9bb5cb37ce3077a0fb14466ae048b3fcd0bd
ksw <seonwongim@gmail.com>
2025-01-18 13:09:03 +0900

    Comparator 구현 및 예제

--- src/main/java/chapter3/_4_method_reference/Main.java
+++ src/main/java/chapter3/_4_method_reference/Main.java
@@ -1,8 +1,10 @@
 package chapter3._4_method_reference;
 
 import common.apple.Apple;
+import common.comparator.AppleComparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.Comparator.comparing;
@@ -17,6 +19,27 @@ public class Main {
 
         inventory.sort(comparing(Apple::getWeight));
 
+        //1단계 코드 전달
+        inventory.sort(new AppleComparator());
 
+
+        //2단계 익명 클래스 사용
+        inventory.sort(new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getWeight().compareTo(o2.getWeight());
+            }
+        });
+
+        //3람다 표현식 사용
+        inventory.sort((o1, o2) -> o1.getWeight().compareTo(o2.getWeight()));
+
+        Comparator<Apple> c = Comparator.comparing((a) -> a.getWeight());
+        inventory.sort(c);
+
+        inventory.sort(Comparator.comparing((a) -> a.getWeight()));
+
+
+        inventory.sort(Comparator.comparing(Apple::getWeight));
     }
 }
--- src/main/java/common/comparator/AppleComparator.java
+++ src/main/java/common/comparator/AppleComparator.java
@@ -1,4 +1,12 @@
 package common.comparator;
 
-public class AppleComparator {
+import common.apple.Apple;
+
+import java.util.Comparator;
+
+public class AppleComparator implements Comparator<Apple> {
+    @Override
+    public int compare(Apple o1, Apple o2) {
+        return o1.getWeight().compareTo(o2.getWeight());
+    }
 }
