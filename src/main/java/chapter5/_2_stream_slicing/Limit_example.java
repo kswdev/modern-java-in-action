dc4a2c792a5987ff5e780f4d0fb6d5f0dc5b0236
ksw <seonwongim@gmail.com>
2025-02-13 23:57:26 +0900

    CHAPTER5 - limit 주석 추가

--- src/main/java/chapter5/_2_stream_slicing/Limit_example.java
+++ src/main/java/chapter5/_2_stream_slicing/Limit_example.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Limit_example {
 
     public static void main(String[] args) {
+        // 처음 n개 요소 추출
         List<Dish> dishes = DishExample.menu.stream()
                 .filter(dish -> dish.getCalories() > 300)
                 .limit(3)
