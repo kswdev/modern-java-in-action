5682e956ca63945960b1aa5768a78dd97891c555
ksw <seonwongim@gmail.com>
2025-02-13 22:15:51 +0900

    칼로리 순으로 순서 변경

--- src/main/java/common/dish/DishExample.java
+++ src/main/java/common/dish/DishExample.java
@@ -6,14 +6,14 @@ import java.util.List;
 public abstract class DishExample {
     public static List<Dish> menu = Arrays.asList(
 
-            new Dish("pork", false, 800, Dish.Type.MEAT),
-            new Dish("beef", false, 700, Dish.Type.MEAT),
-            new Dish("chicken", false, 400, Dish.Type.MEAT),
-            new Dish("french fries", true, 530, Dish.Type.OTHER),
-            new Dish("rice", true, 350, Dish.Type.OTHER),
             new Dish("season fruits", true, 120, Dish.Type.OTHER),
-            new Dish("pizza", true, 550, Dish.Type.OTHER),
             new Dish("prawns", false, 300, Dish.Type.FISH),
-            new Dish("salmon", false, 450, Dish.Type.FISH)
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("salmon", false, 450, Dish.Type.FISH),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("pork", false, 800, Dish.Type.MEAT)
     );
 }
