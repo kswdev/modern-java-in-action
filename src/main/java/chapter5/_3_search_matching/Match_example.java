e50d171e42d68551485d170f265cfb54ed76018e
ksw <seonwongim@gmail.com>
2025-02-14 16:08:04 +0900

    CHAPTER5 - 매칭

---
+++ src/main/java/chapter5/_3_search_matching/Match_example.java
@@ -0,0 +1,23 @@
+package chapter5._3_search_matching;
+
+import common.dish.Dish;
+import common.dish.DishExample;
+
+public class Match_example {
+
+    public static void main(String[] args) {
+
+        //프레디케이트가 적어도 한 요소와 일치하는지 확인
+        if (DishExample.menu.stream().anyMatch(Dish::isVegetarian)) {
+            System.out.println("The menu is somewhat vegetarian friendly!");
+        }
+
+        //프레디케이트가 모든 요소와 일치하는지 확인
+        boolean isHealthy = DishExample.menu.stream()
+                .allMatch(dish -> dish.getCalories() < 1000);
+
+        //프레디케이트와 일치하는 요소가 없는지 확인
+        boolean isHealthy2 = DishExample.menu.stream()
+                .noneMatch(dish -> dish.getCalories() > 1000);
+    }
+}
