4a7a29eaa72cfb2f85e7cf9752bef17388afc57c
ksw <seonwongim@gmail.com>
2025-02-25 21:13:53 +0900

    Add custom ToListCollector and usage example
    Introduce a custom Collector implementation, ToListCollector, to collect stream elements into a List. Also, add an example demonstrating its usage alongside alternative collection approaches in a stream pipeline.

---
+++ src/main/java/chapter6/_5_collector/Collector_example.java
@@ -0,0 +1,45 @@
+package chapter6._5_collector;
+
+import common.collector.ToListCollector;
+import common.dish.Dish;
+import common.dish.DishExample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Collector_example {
+
+
+    public static void main(String[] args) {
+
+        ToListCollector<Integer> collector = new ToListCollector<>();
+
+        //시작
+        List<Integer> integers = collector.supplier().get();
+
+        //스트림에 남아있는 요소가 없을때 까지
+        collector.accumulator().accept(integers, 1);
+
+        //결과
+        List<Integer> result = collector.finisher().apply(integers);
+
+
+        //서브 스트림1
+        List<Integer> result1 = collector.finisher().apply(List.of(1, 2, 3));
+        //서브 스트림2
+        List<Integer> result2 = collector.finisher().apply(List.of(4, 5, 6));
+
+        List<Integer> combinedResults = collector.combiner().apply(result1, result2);
+
+
+        List<Dish> dishes = DishExample.menu.stream()
+                .filter(Dish::isVegetarian)
+                .collect(new ToListCollector<>());
+
+        List<Dish> dishes2 = DishExample.menu.stream()
+                .collect(ArrayList::new
+                        ,ArrayList::add
+                        ,ArrayList::addAll);
+    }
+}
