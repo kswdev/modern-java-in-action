a734d4249ddedb464b58acd3d33429872f073dcb
ksw <seonwongim@gmail.com>
2025-03-02 10:38:59 +0900

    Add ForkJoinSum example to demonstrate Fork/Join framework
    This example showcases the use of the Fork/Join framework for parallel computation by summing numbers with a custom ForkJoinTask. It includes a performance measurement utility to evaluate efficiency.

---
+++ src/jmh/java/chapter7/_2_fork_join_framework/ForkJoinSum_example.java
@@ -0,0 +1,24 @@
+package chapter7._2_fork_join_framework;
+
+import chapter7._2_fork_join_framework.forkJoin.ForkJoinSumCalculator;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.stream.LongStream;
+
+import static common.Measure.measure;
+
+public class ForkJoinSum_example {
+
+    private static final long N = 10_000_000L;
+
+    public static void main(String[] args) {
+        measure(ForkJoinSum_example::forkJoinSum, 10, N);
+    }
+
+    public static long forkJoinSum(long n) {
+        long[] numbers = LongStream.rangeClosed(1, n).toArray();
+        ForkJoinTask<Long> task = new ForkJoinSumCalculator(numbers);
+        return new ForkJoinPool().invoke(task);
+    }
+}
