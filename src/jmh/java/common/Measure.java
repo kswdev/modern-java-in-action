ef25b4e5494808f3a545a02508a50c7f0b6608e7
ksw <seonwongim@gmail.com>
2025-03-02 10:36:25 +0900

    ProperUseOfParallelStream.java
    extract template code from ProperUseOfParallelStream and placed in Measure abstract class

--- src/jmh/java/common/Measure.java
+++ src/jmh/java/common/Measure.java
@@ -1,12 +1,22 @@
 package common;
 
+import chapter7._1_parallel_stream.ProperUseOfParallelStream;
+
 import java.util.function.Function;
 
 public abstract class Measure {
 
-    public static long measurePerf(Function<Long, Long> function, long n) {
+    private static long measurePerf(Function<Long, Long> function, long n) {
         long start = System.currentTimeMillis();
         function.apply(n);
         return System.currentTimeMillis() - start;
+    }
+
+    public static void measure(Function<Long, Long> function, int iterate, long n) {
+        for (int i = 0; i < iterate; i++) {
+            long result = function.apply(n);
+            System.out.println("Result: " + result);
+        }
+        System.out.println("SideEffect parallel sum done in: " + measurePerf(ProperUseOfParallelStream::sideEffectSum, n) + " msecs");
     }
 }
