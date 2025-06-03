b9c6f39e4f6139e5ba0f2aa57da0e473825ba35a
ksw <seonwongim@gmail.com>
2025-06-03 23:18:59 +0900

    17장 리액티브 프로그래밍 2. 리액티브 프로그래밍와 Flow API  - Flow processor 구현(데이터 변환)

--- src/main/java/chapter17/_2_Flow/model/temp/Main.java
+++ src/main/java/chapter17/_2_Flow/model/temp/Main.java
@@ -6,11 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         // 뉴엑에 새 Publisher를 만들고 구독을 시작한다.
-        getTemperature("Seoul").subscribe(new TempSubscriber());
+        //getTemperature("Seoul").subscribe(new TempSubscriber());
+        getCelsiusTemperature("Seoul").subscribe(new TempSubscriber());
     }
 
     private static Publisher<TempInfo> getTemperature(String town) {
         return subscriber -> subscriber.onSubscribe(
                 new TempSubscription(subscriber, town));
+    }
+
+    public static Publisher<TempInfo> getCelsiusTemperature(String town) {
+        return subscriber -> {
+            TempProcessor processor = new TempProcessor();
+            processor.subscribe(subscriber);
+            processor.onSubscribe(new TempSubscription(processor, town));
+        };
     }
 }
---
+++ src/main/java/chapter17/_2_Flow/model/temp/TempProcessor.java
@@ -0,0 +1,35 @@
+package chapter17._2_Flow.model.temp;
+
+import java.util.concurrent.Flow.*;
+
+public class TempProcessor implements Processor<TempInfo, TempInfo> {
+
+    private Subscriber<? super TempInfo> subscriber;
+
+    @Override
+    public void subscribe(Subscriber<? super TempInfo> subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        subscriber.onSubscribe(subscription);
+    }
+
+    @Override
+    public void onNext(TempInfo item) {
+        // 섭씨로 변환한 다음 TempInfo 를 전송
+        subscriber.onNext(new TempInfo(item.getTown(),
+                (item.getTemp() - 32) * 5 / 9));
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        subscriber.onError(throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        subscriber.onComplete();
+    }
+}
